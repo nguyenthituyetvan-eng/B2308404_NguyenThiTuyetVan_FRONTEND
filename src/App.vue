c9dddf91d0fc353bfb55a2205e463c7c1727cb61
Nguyen Thi Tuyet Van <vanb2308404@student.ctu.edu.vn>
2026-02-19 15:52:19 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,31 @@
 <script>
+import AppHeader from "@/components/AppHeader.vue";
+
+export default {
+  components: {
+    AppHeader,
+  },
+};
+</script>
+
+<template>
+  <div id="app">
+    <AppHeader />
+
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
+
+<!-- <script>
 export default {};
 </script>
 <template>
@@ -9,7 +36,8 @@ export default {};
   max-width: 400px;
   margin: auto;
 }
-</style>
+</style> -->
+
 <!-- <script setup>
 import HelloWorld from "./components/HelloWorld.vue";
 import TheWelcome from "./components/TheWelcome.vue";
---
+++ src/components/ContactList.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  props: {
+    contacts: { type: Array, default: [] },
+    activeIndex: { type: Number, default: -1 },
+  },
+  emits: ["update:activeIndex"],
+  methods: {
+    updateActiveIndex(index) {
+      this.$emit("update:activeIndex", index);
+    },
+  },
+};
+</script>
+
+<template>
+  <ul class="list-group">
+    <li
+      class="list-group-item"
+      v-for="(contact, index) in contacts"
+      :key="contact._id"
+      :class="{ active: index === activeIndex }"
+      @click="updateActiveIndex(index)"
+    >
+      {{ contact.name }}
+    </li>
+  </ul>
+</template>
+<style scoped>
+/* Chữ mặc định màu đen */
+.list-group-item {
+  cursor: pointer;
+  color: black !important;
+}
+
+/* Khi được chọn */
+.list-group-item.active {
+  background-color: #0d6efd; /* màu primary của bootstrap */
+  color: white !important;
+}
+</style>
